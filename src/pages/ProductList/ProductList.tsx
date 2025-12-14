fda1e45f8bbf9ef95d68942710917dce9ebe52de
Juan Nonay <juannonay@gmail.com>
2025-12-14 13:32:36 +0100

    feat: add product list and product detail pages

---
+++ src/pages/ProductList/ProductList.tsx
@@ -0,0 +1,11 @@
+import { __ } from '../../i18n/translate'
+
+const ProductList = () => {
+  return (
+    <div>
+      <h2>{__('products.list.title')}</h2>
+    </div>
+  )
+}
+
+export default ProductList
